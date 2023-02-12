466eb0d0fe21e099a2db14ee3918d48af46df5e4
a.semenov <drey_ac@mail.ru>
2023-02-12 23:02:51 +0300

    first home work

---
+++ app/src/main/java/com/example/semenov_lesson2/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.example.semenov_lesson2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
